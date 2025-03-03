fbfa3e245483668b25af10b0e567fb9b39472a1a
agarciaa0422 <agarciaa0422@gmail.com>
2025-03-03 02:06:21 +0000

    word count done

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,25 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name:Arturo
+#Date:3/2/2025
+#Assignment:WordCount
 
 def main():
   textFile = open("gettysberg.txt", 'r')
-  
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+
   for line in textFile:
-    print(line)
-  
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+      for ch in w:
+        letterCount = letterCount + 1
+
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Letters:", letterCount)
 
 if __name__ == '__main__':
   main()
